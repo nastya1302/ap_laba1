3724efeb3f4e4a76418f55252131a7265d904a06
Nastya Mytareva <nastya.mytareva.04@gmail.com>
2023-11-04 21:20:13 +0400

    list images

--- utility.py
+++ utility.py
@@ -1,16 +1,42 @@
 import os
+import time
+import requests
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+#from selenium.webdriver.common.keys import Keys
 
 def creating_folders(names: list):
-    if not os.path.isdir("dataset"):
-        os.mkdir("dataset")
-    os.chdir("dataset")
     if not os.path.isdir(names[0]) and not os.path.isdir(names[1]):
         os.makedirs(names[0])
         os.makedirs(names[1])
 
+def get_images(name: str) -> list:
+    os.chdir(name)
+    url = f"https://yandex.ru/images/search?text={name}"
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get(url)
+    time.sleep(10)
+    driver.find_element(By.CSS_SELECTOR, 'div.serp-item__preview a.serp-item__link').click()
+    list = []
+    for i in range(10):
+            try:
+                time.sleep(0.5)
+                link = driver.find_element(By.CSS_SELECTOR, "a.Button2_view_action").get_attribute("href")
+                list.append(link)
+                driver.find_element(By.CSS_SELECTOR, "div.CircleButton:nth-child(4)").click()
+            except:
+                continue
+    driver.quit()
+
 def main() -> None:
     name1, name2 = "rose", "tulip"
+    if not os.path.isdir("dataset"):
+        os.mkdir("dataset")
+    os.chdir("dataset")
     creating_folders((name1, name2))
+    get_images(name1)
+    #print("Текущая директория изменилась на folder:", os.getcwd())
 
 if __name__ == "__main__":
     main()
