e8c7b53724874236e945fb4d0f9fe9763ba42aed
Nastya Mytareva <nastya.mytareva.04@gmail.com>
2023-11-09 10:01:39 +0400

    final version

--- utility.py
+++ utility.py
@@ -1,22 +1,26 @@
 import os
-import time
-import requests
+from time import sleep
+from typing import List
+
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+import requests
 
-def creating_folders(name: str):
+
+def creating_folders(name: str) -> None:
     if not os.path.isdir(name):
         os.mkdir(name)
 
-def download_images(links: list, name: str):
+
+def download_images(links: List[str], name: str) -> None:
     creating_folders(f"dataset/{name}")
     count = 0
     page = ''
     for link in links:
         while page == '':
             try:
-                time.sleep(5)
+                sleep(5)
                 response = requests.get(link, verify=False).content
                 with open(f'dataset/{name}/{str(count).zfill(4)}.jpg', "wb") as f:
                     f.write(response)
@@ -24,35 +28,35 @@ def download_images(links: list, name: str):
                 break
             except: 
                 print("Error.Waiting 6s")
-                time.sleep(6)
+                sleep(6)
                 print("Now try reconnecting")
                 continue
 
-def get_images(name: str) -> list:
-    url = f"https://yandex.ru/images/search?text={name}"
+
+def get_images(name: str, num_img: int) -> List[str]:
+    url = f'https://yandex.ru/images/search?text={name}'
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get(url)
-    time.sleep(1)
-    driver.find_element(By.CSS_SELECTOR, 'div.serp-item__preview a.serp-item__link').click()
+    driver.find_element(By.CSS_SELECTOR, 'a.serp-item__link').click()
     elem1 = driver.find_element(By.CLASS_NAME, "MMImage-Origin").get_attribute('src')
-    list = [elem1]
-    for i in range(1000):
-            try:
-                time.sleep(1)
-                button = driver.find_element(By.CLASS_NAME, "MediaViewer_theme_fiji-ButtonNext").click()                
-                link = driver.find_element(By.CLASS_NAME, "MMImage-Origin").get_attribute('src')
-                list.append(link)
-            except:
-                continue
+    links = [elem1]
+    for _ in range(num_img):
+        try:
+            sleep(1)
+            driver.find_element(By.CLASS_NAME, "MediaViewer_theme_fiji-ButtonNext").click()                
+            link = driver.find_element(By.CLASS_NAME, "MMImage-Origin").get_attribute('src')
+            links.append(link)
+        except:
+            continue
     driver.close()
-    return list
+    return links
 
-def main() -> None:
-    name1, name2 = "rose", "tulip"
+
+def main(name1: str, name2: str, num_img: int) -> None:
     creating_folders("dataset")
-    download_images(get_images(name1), name1)
-    time.sleep(5)
-    download_images(get_images(name2), name2)
+    download_images(get_images(name1, num_img), name1)
+    download_images(get_images(name2, num_img), name2)
+ 
     
 if __name__ == "__main__":
-    main()
+    main('rose', 'tulip', 9)
