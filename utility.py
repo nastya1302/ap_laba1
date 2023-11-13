10f147ff273ca293faa4dcd35dd2f342ad1a5a3d
Nastya Mytareva <nastya.mytareva.04@gmail.com>
2023-11-13 13:55:52 +0400

    editing the path, adding type-hinting and dosctring

--- utility.py
+++ utility.py
@@ -9,22 +9,30 @@ import requests
 
 
 def creating_folders(name: str) -> None:
+    """
+    The function takes the class name as input and creates a directory if one does not exist at the moment.
+    """
     if not os.path.isdir(name):
         os.mkdir(name)
 
 
 def download_images(links: List[str], name: str) -> None:
+    """
+    The function accepts the class name and a list of links to images as input. 
+    A directory with the name of the class is created and, going through the list, 
+    it downloads images and saves them in the specified directory.
+    """
     creating_folders(f"dataset/{name}")
-    count = 0
-    page = ''
+    count:int = 0
+    page:str = ''
     for link in links:
         while page == '':
             try:
                 sleep(5)
                 response = requests.get(link, verify=False).content
-                with open(f'dataset/{name}/{str(count).zfill(4)}.jpg', "wb") as f:
+                with open(os.path.join('dataset',name,str(count).zfill(4),'.jpg'), "wb") as f:
                     f.write(response)
-                count = count+1
+                count+=1
                 break
             except: 
                 print("Error.Waiting 6s")
@@ -34,17 +42,21 @@ def download_images(links: List[str], name: str) -> None:
 
 
 def get_images(name: str, num_img: int) -> List[str]:
-    url = f'https://yandex.ru/images/search?text={name}'
+    """
+    The function takes as input the name of the class and the number of images to be parsed.
+    A link opens in the browser and with the help of the element code there are links to images that are saved to the list.
+    """
+    url:str = f'https://yandex.ru/images/search?text={name}'
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get(url)
     driver.find_element(By.CSS_SELECTOR, 'a.serp-item__link').click()
-    elem1 = driver.find_element(By.CLASS_NAME, "MMImage-Origin").get_attribute('src')
-    links = [elem1]
+    elem1:str = driver.find_element(By.CLASS_NAME, "MMImage-Origin").get_attribute('src')
+    links:List[str] = [elem1]
     for _ in range(num_img):
         try:
             sleep(1)
             driver.find_element(By.CLASS_NAME, "MediaViewer_theme_fiji-ButtonNext").click()                
-            link = driver.find_element(By.CLASS_NAME, "MMImage-Origin").get_attribute('src')
+            link:str = driver.find_element(By.CLASS_NAME, "MMImage-Origin").get_attribute('src')
             links.append(link)
         except:
             continue
