81d7b1734c5c459decfd6e5e8980c0df60f9f199
Nastya Mytareva <nastya.mytareva.04@gmail.com>
2023-11-18 22:31:20 +0400

    adjustment for pip8

--- utility.py
+++ utility.py
@@ -18,23 +18,25 @@ def creating_folders(name: str) -> None:
 
 def download_images(links: List[str], name: str) -> None:
     """
-    The function accepts the class name and a list of links to images as input. 
-    A directory with the name of the class is created and, going through the list, 
+    The function accepts the class name and a list of links to images as input.
+    A directory with the name of the class is created and, going through the list,
     it downloads images and saves them in the specified directory.
     """
-    creating_folders(os.path.join('dataset', name))
-    count:int = 0
-    page:str = ''
+    creating_folders(os.path.join("dataset", name))
+    count: int = 0
+    page: str = ""
     for link in links:
-        while page == '':
+        while page == "":
             try:
                 sleep(5)
                 response = requests.get(link, verify=False).content
-                with open(os.path.join('dataset', name, f'{str(count).zfill(4)}.jpg'), "wb") as f:
+                with open(
+                    os.path.join("dataset", name, f"{str(count).zfill(4)}.jpg"), "wb"
+                ) as f:
                     f.write(response)
-                count+=1
+                count += 1
                 break
-            except: 
+            except:
                 print("Error.Waiting 6s")
                 sleep(6)
                 print("Now try reconnecting")
@@ -46,17 +48,23 @@ def get_images(name: str, num_img: int) -> List[str]:
     The function takes as input the name of the class and the number of images to be parsed.
     A link opens in the browser and with the help of the element code there are links to images that are saved to the list.
     """
-    url:str = f'https://yandex.ru/images/search?text={name}'
+    url: str = f"https://yandex.ru/images/search?text={name}"
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get(url)
-    driver.find_element(By.CSS_SELECTOR, 'a.serp-item__link').click()
-    elem1:str = driver.find_element(By.CLASS_NAME, "MMImage-Origin").get_attribute('src')
-    links:List[str] = [elem1]
+    driver.find_element(By.CSS_SELECTOR, "a.serp-item__link").click()
+    elem1: str = driver.find_element(By.CLASS_NAME, "MMImage-Origin").get_attribute(
+        "src"
+    )
+    links: List[str] = [elem1]
     for _ in range(num_img):
         try:
             sleep(1)
-            driver.find_element(By.CLASS_NAME, "MediaViewer_theme_fiji-ButtonNext").click()                
-            link:str = driver.find_element(By.CLASS_NAME, "MMImage-Origin").get_attribute('src')
+            driver.find_element(
+                By.CLASS_NAME, "MediaViewer_theme_fiji-ButtonNext"
+            ).click()
+            link: str = driver.find_element(
+                By.CLASS_NAME, "MMImage-Origin"
+            ).get_attribute("src")
             links.append(link)
         except:
             continue
@@ -68,7 +76,7 @@ def main(name1: str, name2: str, num_img: int) -> None:
     creating_folders("dataset")
     download_images(get_images(name1, num_img), name1)
     download_images(get_images(name2, num_img), name2)
- 
-    
+
+
 if __name__ == "__main__":
-    main('rose', 'tulip', 9)
+    main("rose", "tulip", 9)
